49052100be9e86c45e98fda2c089af9a6ca9b72d
meokg456 <meokg456@gmail.com>
2024-08-30 16:43:07 +0700

    Fix invalid baseUrl on Dio

--- lib/dio_config/dio_config.dart
+++ lib/dio_config/dio_config.dart
@@ -6,6 +6,6 @@ part 'dio_config.g.dart';
 @riverpod
 Dio dio(DioRef ref) {
   final dio = Dio();
-  dio.options.baseUrl = 'localhost:8080';
+  dio.options.baseUrl = 'https://www.google.com/';
   return dio;
 }
